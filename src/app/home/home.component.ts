e26e5f77ed8c9d9b86667d38dd184209de515cfc
Aran-tm <armandortmontes@gmail.com>
2024-05-08 15:20:31 -0500

    borrando importaciones innecesarias

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
