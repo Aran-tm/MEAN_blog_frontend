ed5c919bd99d24358247c4a73c4fe5d9d0ef7d43
Aran-tm <armandortmontes@gmail.com>
2024-05-09 12:30:39 -0500

    agregando router links y navegacion entre cmponentes, register, login and home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],    // trabajo con este modulo para poder utilizar el routerLink y navegar entre componentes
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
