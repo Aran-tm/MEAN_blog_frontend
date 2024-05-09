10a4405f9f0cb121d75a8b2760832cb5962d72b3
Aran-tm <armandortmontes@gmail.com>
2024-05-09 13:09:00 -0500

    page 404 updated

--- src/app/page404/page404.component.ts
+++ src/app/page404/page404.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-page404',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './page404.component.html',
   styleUrl: './page404.component.css'
 })
